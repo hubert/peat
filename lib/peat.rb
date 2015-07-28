88f9da0cbc73f4d8ad9e44230c1f01db4798c305
Hubert Huang <hubert77@gmail.com>
2015-07-28 16:04:42 -0700

    Allow configuration of client to use custom faraday middleware

--- lib/peat.rb
+++ lib/peat.rb
@@ -3,6 +3,7 @@ require 'faraday_middleware'
 
 module Peat
   class MissingConfiguration < Exception; end
+  class DeliveryFailed < RuntimeError; end
 
   autoload :TokenManager, 'peat/token_manager'
   autoload :Client, 'peat/client'
--- lib/peat/client.rb
+++ lib/peat/client.rb
@@ -1,18 +1,22 @@
 module Peat
-  module Client
-    module_function
+  class Client
+
+    def initialize(middleware: [], adapter: Faraday.default_adapter)
+      @middleware = middleware
+      @adapter = adapter
+    end
 
     def connection
       @connection ||= Faraday.new(url: "https://www.exacttargetapis.com/messaging/v1/") do |conn|
         conn.request :json
 
-        conn.response :logger
+        @middleware.each { |m| conn.use m }
 
-        conn.adapter Faraday.default_adapter
+        conn.adapter @adapter
       end
     end
 
-    def deliver(trigger_name, params) 
+    def deliver(trigger_name, params)
       resp = connection.post do |req|
         req.url "messageDefinitionSends/key:#{trigger_name}/send"
         req.headers['Content-Type'] = 'application/json'
@@ -21,6 +25,10 @@ module Peat
       end
 
       resp.success?
+    end
+
+    def deliver!(trigger_name, params)
+      deliver(trigger_name, params) or raise Peat::DeliveryFailed
     end
   end
 end
